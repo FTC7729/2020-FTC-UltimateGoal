50fb166c1c53d21ae4f555eb7533a65ceac7f050
Ike C <>
2020-10-08 20:08:31 -0400

    Enabled Autonomous.
    Enabled Autonomous because I forgot to in the first commit sorry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_DriveByEncoder_Example.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_DriveByEncoder_Example.java
@@ -49,7 +49,6 @@ import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Example;
 // CHAWKS: Name it something useful!
 @Autonomous(name="RedZone Right 1 Test", group="Red")
 // CHAWKS: What does @Disabled mean? what happens if we remove it?
-@Disabled
 public class Auto_DriveByEncoder_Example extends LinearOpMode {
 
     /* CHAWKS: Call and declare the robot here */
