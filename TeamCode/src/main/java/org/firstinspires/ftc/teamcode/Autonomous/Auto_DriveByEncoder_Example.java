a7c298a503386696329ab533fc9a138601ed6a44
Ike C <>
2020-10-13 20:08:53 -0400

    Enabled distance Sensor. Edited autonomous.
    This has been a very productive day in terms of looking at java, I read
    and enabled the 2m distanceSensor example file script.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_DriveByEncoder_Example.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_DriveByEncoder_Example.java
@@ -105,6 +105,8 @@ public class Auto_DriveByEncoder_Example extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        robot.leftBack.setPower(1.0);
+        robot.rightBack.setPower(1.0);
         encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
