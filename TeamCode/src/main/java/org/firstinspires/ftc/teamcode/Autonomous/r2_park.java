7f17908f821fe4781cf5ee199dad98d7e901b962
Gavin M <>
2020-10-31 10:29:38 -0400

    initial r2 auto mode with pseudo code
    skelteon of r2 auto park foir 5 points.
    more to come soon.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/r2_park.java
@@ -0,0 +1,128 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Example;
+
+//import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+// List where other files are located that are used in this OpMode
+
+/**
+ * In this example:
+ * This file illustrates the concept of driving OUR robot in HardwareMap_Example
+ *
+ */
+// CHAWKS: Name it something useful!
+@Autonomous(name="r2_park", group="Red")
+// CHAWKS: What does @Disabled mean? what happens if we remove it?
+
+public class r2_park extends HardwareMap_Example {
+ /*
+        CHAWKS: All the values can be moved to HardwareMap? Are these common values?
+     */
+
+    /*
+        CHAWKS: It has begun!!! Run the OpMode!!! Make the robot execute all our code!!!
+    */
+
+    // MUST HAVE
+    @Override
+    public void runOpMode() {
+
+
+        // Initialize the drive system variables.
+        // The init() method of the hardware class does all the work here
+        // Initialize the drive system variables.
+        // The init() method of the hardware class does all the work here
+
+        /*
+            CHAWKS: On Driver Station, telemetry will be display!
+                    Why is this good for the Drivers?
+        */
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status: ", "Hit [Init] to Initialize ze bot");    //
+        telemetry.update();
+
+        /*
+            CHAWKS: Step 0. Initialize OUR ROBOT
+        */
+        // MUST HAVE THIS LINE BELOW
+
+
+
+        int state = 0;
+        if (state == 0){
+            //init robot
+            init(hardwareMap);
+            waitForStart();
+            state = 1;
+        }
+
+        if (state == 1){
+            telemetry.addData("State","1");
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED,-12,-12, 30);
+            //facing west move forward one foot
+            state = 2;
+        }
+
+        if (state == 2) {
+            telemetry.addData("State","2");
+            telemetry.update();
+            encoderDrive(TURN_SPEED,-9,9, 30);
+            //turn 90 degrees clockwise
+            state = 3;
+        }
+
+        if (state == 3) {
+            telemetry.addData("State", "3");
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED,-72,-72,30);
+            //move forward six feet
+            state = 4;
+        }
+
+        if (state == 4) {
+            telemetry.addData("State","4");
+            telemetry.update();
+            rightBack.setPower(0);
+            leftBack.setPower(0);
+            //stop
+
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/HardwareMap_Example.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/HardwareMap_Example.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.HardwareMap;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -51,12 +52,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Motor channel:  Left Back (LB) drive motor:        "leftBack"
  * Motor channel:  Right Back (RB) drive motor:        "rightBack"
  */
-public class HardwareMap_Example
+abstract public class HardwareMap_Example extends LinearOpMode
 {
     /* Public OpMode members. */
     // CHAWKS: The Robot Parts need to be established here
-    public DcMotor  leftFront   = null;
-    public DcMotor  rightFront  = null;
+    // public DcMotor  leftFront   = null;
+    // public DcMotor  rightFront  = null;
     public DcMotor  leftBack   = null;
     public DcMotor  rightBack  = null;
     /////////////////////////////////////
@@ -64,11 +65,21 @@ public class HardwareMap_Example
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
     private ElapsedTime period  = new ElapsedTime();
+    /* CHAWKS: Call and declare the robot here */
+    private ElapsedTime     runtime = new ElapsedTime();
 
     /* Constructor */
     public HardwareMap_Example(){
 
     }
+
+    public static final double     COUNTS_PER_MOTOR_REV    = 288 ;    // eg: Rev Core Hex Motor Encoder
+    public static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // This is < 1.0 if geared UP
+    public static final double     WHEEL_DIAMETER_INCHES   = 3.5 ;     // For figuring circumference
+    public static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * Math.PI);
+    public static final double     DRIVE_SPEED             = 1;
+    public static final double     TURN_SPEED              = 1;
 
     // Initialize standard Hardware interfaces
     /*
@@ -84,8 +95,8 @@ public class HardwareMap_Example
             CHAWKS: The deviceName should ALWAYS ALWAYS ALWAYS
                     match the part name to avoid confusion
          */
-        leftFront  = hwMap.get(DcMotor.class, "leftFront");
-        rightFront  = hwMap.get(DcMotor.class, "rightFront");
+        // leftFront  = hwMap.get(DcMotor.class, "leftFront");
+        // rightFront  = hwMap.get(DcMotor.class, "rightFront");
         leftBack = hwMap.get(DcMotor.class, "leftBack");
         rightBack = hwMap.get(DcMotor.class, "rightBack");
 
@@ -94,8 +105,8 @@ public class HardwareMap_Example
         /*
             CHAWKS: Why are we reversing the Right Wheels?
          */
-        leftFront.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightFront.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        //leftFront.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        //rightFront.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         leftBack.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightBack.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
@@ -103,8 +114,8 @@ public class HardwareMap_Example
         /*
             CHAWKS: Why do we set the power to zero?
          */
-        leftFront.setPower(0);
-        rightFront.setPower(0);
+        //leftFront.setPower(0);
+        //rightFront.setPower(0);
         leftBack.setPower(0);
         rightBack.setPower(0);
 
@@ -113,13 +124,80 @@ public class HardwareMap_Example
             CHAWKS: Encoder Exercise!
          */
 
-        //leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+    }
+    /*
+     *  CHAWKS: It's a METHOD!!!
+     *
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        /*
+            CHAWKS: opModeIsActive is a very awesome & important
+                    Autonomous mode is 30 seconds...
+         */
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = leftBack.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightBack.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftBack.setTargetPosition(newLeftTarget);
+            rightBack.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftBack.setPower(Math.abs(speed));
+            rightBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftBack.isBusy() && rightBack.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        leftBack.getCurrentPosition(),
+                        rightBack.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftBack.setPower(0);
+            rightBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
     }
  }
 
