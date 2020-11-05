6f413fb7b6714f9ed5b7ab74bc139b30a7488c40
Coach Cassie <>
2020-11-04 22:43:09 -0500

    TeleDrive Locally with Jersey Bot
    Users must pull the TeleDrive git repo and run the Windows applications.
    More details soon on confluence.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive/SampleTeleDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive/SampleTeleDriveOpMode.java
@@ -1,26 +1,61 @@
 package org.firstinspires.ftc.teamcode.TeleDrive; //set your package
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Example;
 import org.firstinspires.ftc.teamcode.TeleDrive.TeleDrive;
 
+import java.net.CookieHandler;
+
 @TeleOp(name = "Your TeleOp Name", group = "TeleOpMode")
+@Disabled
 public class SampleTeleDriveOpMode extends TeleDrive {
+
+
     private ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-    /* CHAWKS: Call and declare the robot here */
-    HardwareMap_Example robot   = new HardwareMap_Example();    // Use the Example hardware map
+    public DcMotor leftFront   = null;
+    public DcMotor rightFront   = null;
+    public DcMotor leftBack   = null;
+    public DcMotor rightBack   = null;
+
+    // private HardwareMap ahwMap;
 
     @Override
     public void init() {
         super.init();
         //Init code here
-                /*
-            CHAWKS: Step 0. Initialize OUR ROBOT
-        */
-        // MUST HAVE THIS LINE BELOW
-        robot.init(hardwareMap);
+        /* local OpMode members. */
+        //HardwareMap hwMap           =  null;
+
+
+        // Define and Initialize Motors
+        /*
+            CHAWKS: The deviceName should ALWAYS ALWAYS ALWAYS
+                    match the part name to avoid confusion
+
+*/
+
+        leftFront  = hardwareMap.dcMotor.get("leftFront");
+        rightFront  = hardwareMap.dcMotor.get("rightFront");
+
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+
+
+        // Set Direction/Motion for Motors
+        /*
+            CHAWKS: Why are we reversing the Right Wheels?
+
+*/
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
     }
 
     public void init_loop() {
@@ -39,48 +74,21 @@ public class SampleTeleDriveOpMode extends TeleDrive {
     public void loop() {
         super.loop();
         //Loop code here
-
-        // Below are VARIABLES that must be outside the "while (opModeIsActive())" loop
-        double left;
-        double right;
-        double drive;
-        double turn;
-        double max;
-        drive = -gamepad1.left_stick_y;
-        turn  =  gamepad1.right_stick_x;
-
-        // Combine drive and turn for blended motion.
-        left  = drive + turn;
-        right = drive - turn;
-
-        // Normalize the values so neither exceed +/- 1.0
-        max = Math.max(Math.abs(left), Math.abs(right));
-        if (max > 1.0)
-        {
-            left /= max;
-            right /= max;
-        }
-
-        // Output the safe vales to the motor drives.
-            /*
-                CHAWKS: Put in the power number!
-                        Which motors are we missing?
-                        What would happen if you apply power to half the wheels?
-             */
-        robot.leftBack.setPower(left);
-        robot.rightBack.setPower(right);
-
-        // Use gamepad left & right Bumpers to open and close the claw
-        //if (gamepad1.right_bumper)
-        //clawOffset += CLAW_SPEED;
-        //else if (gamepad1.left_bumper)
-        //clawOffset -= CLAW_SPEED;
-
+        handleGamepad1(gamepad1);
+        handleGamepad2(gamepad2);
     }
 
     @Override
     public void stop() {
         super.stop();
         //Stop code here
+    }
+
+    public void handleGamepad1(Gamepad gamepad) {
+
+    }
+
+    public void handleGamepad2(Gamepad gamepad) {
+
     }
 }
