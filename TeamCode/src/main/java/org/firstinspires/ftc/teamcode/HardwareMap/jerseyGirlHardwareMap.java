12b93c418a20e2f0c5a3f7b1e45dfb00d775d385
unknown <oliverastor9139@gmail.com>
2020-11-21 10:42:01 -0500

    After fixing the wiring error we corrected the HW

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/jerseyGirlHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/jerseyGirlHardwareMap.java
@@ -89,10 +89,10 @@ public abstract class jerseyGirlHardwareMap extends LinearOpMode {
         /*
             CHAWKS: Why are we reversing the Right Wheels?
          */
-        leftFront.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        rightFront.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        leftBack.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        rightBack.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        leftFront.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightFront.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        leftBack.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightBack.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to ZERO! power
         /*
