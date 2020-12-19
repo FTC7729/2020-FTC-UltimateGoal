f33220b28f97490debb66175a68c48ad6b109428
Gavin M <>
2020-12-19 09:48:29 -0500

    Added the stop function to TeleDrive dpad controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive/JerseyGirl_TeleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive/JerseyGirl_TeleDrive.java
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode.TeleDrive;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+
 @TeleOp(name = "Jersey TeleDrive ", group = "B")
 //@Disabled
 public class JerseyGirl_TeleDrive extends JerseyGirl_TeleDriveHandler {
 
     double speedScale = 0.25;
+
     public void goForward(double power) {
         leftFront.setPower(power);
         rightFront.setPower(power);
@@ -15,9 +17,10 @@ public class JerseyGirl_TeleDrive extends JerseyGirl_TeleDriveHandler {
         rightBack.setPower(power);
     }
 
-    public void goBackward(double power){
+    public void goBackward(double power) {
         goForward(-power);
     }
+
     public void strafeLeft(double power) {
         leftFront.setPower(-power);
         rightFront.setPower(power);
@@ -31,22 +34,63 @@ public class JerseyGirl_TeleDrive extends JerseyGirl_TeleDriveHandler {
         leftBack.setPower(-power);
         rightBack.setPower(power);
     }
-    public void handleGamepad1(Gamepad gamepad){
-       double rStickX;
-       double rStickY;
-       double lStickX;
-       double targetAngle;
-       double mag1;
-       double mag2;
-       double rotationPower;
-       double maxPower;
-       double scaleDown;
-       boolean dpadLeft;
-       boolean dpadRight;
-       boolean dpadUp;
-       boolean dpadDown;
+    public final int LIFT_MAX_POS = 6618;
 
+    public final int LIFT_MIN_POS = 0;
 
+    public final int LIFT_ON_FOUNDATION = 770;
+
+    public final int CLAW_MAX_POS = 5000;
+
+    public final int CLAW_MIN_POS = 0;
+
+    public final double ROTATION_CONSTANT = 0.4;
+
+    double subtractAngle = 0;
+
+    double prevTime = 0;
+    double startTime = System.currentTimeMillis();
+    double prevLStick = 0;
+    double prevMag1 = 0;
+    double prevMag2 = 0;
+    double timeSinceLastIncrement = 0;
+    final double timeIncrement = 50;
+    final double powIncrement = 0.2;
+    final double deadZone = 0.05;
+
+    public void handleGamepad1(Gamepad gamepad) {
+        double rStickX;
+        double rStickY;
+        double lStickX = 0;
+        double targetAngle;
+        double mag1;
+        double mag2;
+        double rotationPower;
+        double maxPower;
+        double scaleDown;
+        double currentAngle;
+        double totalAngle = 0;
+        boolean lBumper;
+        boolean rBumper;
+        boolean dpadLeft;
+        boolean dpadRight;
+        boolean dpadUp;
+        boolean dpadDown;
+        boolean aPress;
+
+        int numOfIncrements = 0;
+        //we have to increment later, so we save how often we do it now
+        while (timeSinceLastIncrement > timeIncrement) {
+            timeSinceLastIncrement -= timeIncrement;
+            //changes the power of motors
+            if (lStickX > prevLStick) {
+                prevLStick += powIncrement;
+            } else if (lStickX < prevLStick) {
+                prevLStick -= powIncrement;
+            }
+
+            numOfIncrements++;
+        }
         rStickX = gamepad.right_stick_x * speedScale;
         rStickY = -gamepad.right_stick_y * speedScale;
         lStickX = gamepad.left_stick_x * speedScale;
@@ -56,30 +100,64 @@ public class JerseyGirl_TeleDrive extends JerseyGirl_TeleDriveHandler {
         dpadDown = gamepad.dpad_down;
 
         targetAngle = (Math.atan2(rStickY, rStickX));
-        if(dpadLeft){
+        if (dpadLeft) {
             strafeLeft(0.5);
-        } else if(dpadRight){
+        } else if (dpadRight) {
             strafeRight(0.5);
-        } else if(dpadUp){
+        } else if (dpadUp) {
             goForward(0.5);
-        } else if(dpadDown) {
+        } else if (dpadDown) {
             goBackward(0.5);
+        } else {
+
+
+            rotationPower = -lStickX * ROTATION_CONSTANT;
+            mag1 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(totalAngle + Math.PI / 4));
+            mag2 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(totalAngle - Math.PI / 4));
+
+            maxPower = Math.max(Math.abs(mag1) + Math.abs(rotationPower), Math.abs(mag2) + Math.abs(rotationPower));
+
+            //adjusts mag1 and 2 so that it's smooth
+            for (int i = 0; i < numOfIncrements; i++) {
+                if (mag1 > prevMag1) {
+                    prevMag1 += powIncrement;
+                } else if (mag1 < prevMag1) {
+                    prevMag1 -= powIncrement;
+                }
+
+                if (mag2 > prevMag2) {
+                    prevMag2 += powIncrement;
+                } else if (mag2 < prevMag2) {
+                    prevMag2 -= powIncrement;
+                }
+            }
+
+            if (Math.abs(prevMag1) < deadZone) {
+                prevMag1 = 0;
+            }
+            if (Math.abs(prevMag2) < deadZone) {
+                prevMag2 = 0;
+            }
+
+            mag1 = prevMag1;
+            mag2 = prevMag2;
+
+            telemetry.addData("mag1: ", "%.3f", mag1);
+            telemetry.addData("mag2: ", "%.3f", mag2);
+            telemetry.addData("rotationPower: ", "%.3f", rotationPower);
+
+            if (maxPower > 1.0)
+                scaleDown = 1.0 / maxPower;
+            else
+                scaleDown = 1.0;
+
+
+            leftFront.setPower((mag2 - rotationPower) * scaleDown);
+            leftBack.setPower((mag1 - rotationPower) * scaleDown);
+            rightBack.setPower((mag2 + rotationPower) * scaleDown);
+            rightFront.setPower((mag1 + rotationPower) * scaleDown);
         }
-        rotationPower = -lStickX;
-        mag1 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(targetAngle + Math.PI / 4));
-        mag2 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(targetAngle - Math.PI / 4));
-
-        maxPower = Math.max(Math.abs(mag1) + Math.abs(rotationPower), Math.abs(mag2) + Math.abs(rotationPower)) + 0.15;
-        scaleDown = 1.0;
-
-        if (maxPower > 1)
-            scaleDown = 1.0 / maxPower;
-
-        leftFront.setPower((mag2 - rotationPower) * scaleDown);
-        leftBack.setPower((mag1 - rotationPower) * scaleDown);
-        rightBack.setPower((mag2 + rotationPower) * scaleDown);
-        rightFront.setPower((mag1 + rotationPower) * scaleDown);
-
     }
-
 }
+
+
