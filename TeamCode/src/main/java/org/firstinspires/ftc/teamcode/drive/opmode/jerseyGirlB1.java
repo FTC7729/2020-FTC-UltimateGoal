5e861fe0c8af32878b17e371e988ee6347397ca3
Coach Cassie <>
2021-01-30 10:43:43 -0500

    added new roadrunner auto Prog. jerseyGirlB1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/jerseyGirlB1.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.apache.commons.math3.ml.distance.DistanceMeasure;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+/*
+ * This is a simple routine to test translational drive capabilities.
+ */
+@Config
+@Autonomous(group = "drive")
+public class jerseyGirlB1 extends LinearOpMode {
+    public static double DISTANCE = 24; // in
+    public static double DISTANCE2 = 72;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory trajectory1 = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(DISTANCE)
+                .build();
+        Trajectory trajectory2 = drive.trajectoryBuilder(new Pose2d())
+                .forward(DISTANCE2)
+                .build();
+//        Trajectory trajectoryRight = drive.trajectoryBuilder(new Pose2d())
+ //               .strafeRight(DISTANCE)
+//                .build();
+        waitForStart();
+
+
+        if (isStopRequested()) return;
+
+        drive.followTrajectory(trajectory1);
+        drive.followTrajectory(trajectory2);
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("finalX", poseEstimate.getX());
+        telemetry.addData("finalY", poseEstimate.getY());
+        telemetry.addData("finalHeading", poseEstimate.getHeading());
+        telemetry.update();
+
+        while (!isStopRequested() && opModeIsActive()) ;
+    }
+}
